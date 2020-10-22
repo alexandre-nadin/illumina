8eb9d1a7afd04cd7ecf247295a85335f3aaaf26d
Alexandre Nadin <alex.nadin12@gmail.com>
2020-10-22 18:31:52 +0200

    [cosr/bin/cosr] Updated new paths.

--- cosr/bin/cosr.sh
+++ cosr/bin/cosr.sh
@@ -2,8 +2,8 @@
 COSR_TMP_DIR="/tmp"
 COSR_DEMUX_RAW_DIR="/beegfs/scratch/demux/ric.cosr"
 COSR_ILLUMINA_DIR="/sequencing/ric.cosr"
-COSR_SSHEET_DIR="/lustre1/SampleSheets"
-COSR_STD_ANALYSIS_DIR="/lustre2/standard-analysis"
+COSR_SSHEET_DIR="/beegfs/scratch/demux/ric.cosr"
+COSR_STD_ANALYSIS_DIR="/beegfs/scratch/standard-analysis"
 COSR_STD_PRJ_DIR="${COSR_STD_ANALYSIS_DIR}/projects"
 COSR_STD_SEQRUN_DIR="${COSR_STD_ANALYSIS_DIR}/sequencing-runs"
 COSR_UNREMOVABLE_FLAG='.unremovable'
