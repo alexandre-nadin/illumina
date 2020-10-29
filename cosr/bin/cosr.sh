fa8a96557edb8329c1cfc721461d7f9abfb32ada
Alexandre Nadin <alex.nadin12@gmail.com>
2020-10-29 18:25:18 +0100

    [cosr] Added cosr-nx submodule.

--- cosr/bin/cosr.sh
+++
@@ -1,9 +0,0 @@
-# bash
-COSR_TMP_DIR="/tmp"
-COSR_DEMUX_RAW_DIR="/beegfs/scratch/demux/ric.cosr"
-COSR_ILLUMINA_DIR="/sequencing/ric.cosr"
-COSR_SSHEET_DIR="/beegfs/scratch/demux/ric.cosr/samplesheets"
-COSR_STD_ANALYSIS_DIR="/beegfs/scratch/standard-analysis"
-COSR_STD_PRJ_DIR="${COSR_STD_ANALYSIS_DIR}/projects"
-COSR_STD_SEQRUN_DIR="${COSR_STD_ANALYSIS_DIR}/sequencing-runs"
-COSR_UNREMOVABLE_FLAG='.unremovable'
